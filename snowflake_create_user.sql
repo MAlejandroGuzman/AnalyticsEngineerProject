7b590d414e40c2791461eb6c144302cbbc95d6ec
MAlejandroGuzman <alejandroguzman.data@gmail.com>
2025-03-31 18:24:26 -0600

    Create snowflake_create_user.sql

---
+++ snowflake_create_user.sql
@@ -0,0 +1,17 @@
+snowlflake create DB example:
+
+use role accountadmin;
+
+create warehouse dbt_wh with warehouse_size='x-small';
+create database if not exists dbt_db;
+create role if not exists dbt_role;
+
+show grants on warehouse dbt_wh;
+
+grant role dbt_role to user DBT;
+grant usage on warehouse dbt_wh to role dbt_role;
+grant all on database dbt_db to role dbt_role;
+
+use role dbt_role;
+
+create schema if not exists dbt_db.dbt_schema;
